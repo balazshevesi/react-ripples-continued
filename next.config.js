6764ea6146b38de1340ad4fccfa16e673b9bd44d
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-10-08 17:08:09 +0200

    Edit config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
 
-module.exports = nextConfig
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
